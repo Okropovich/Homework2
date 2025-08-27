db0637ec1d340cca15e5c4b18cc9baba21b6674a
Okropovich <89385037063k@gmail.com>
2025-08-27 03:46:02 +0300

    Переделка.

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var dog = 0.8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println("Собака "  + dog );
-        System.out.println("Кот "  + cat );
-        System.out.println("бумага "  + paper );
+        System.out.println("Собака " + dog);
+        System.out.println("Кот " + cat);
+        System.out.println("бумага " + paper);
 
         dog += 4;
         cat += 4;
         paper += 4;
-        System.out.println("Собака "+dog);
-        System.out.println("Кот "+cat);
-        System.out.println("Бумага "+ paper);
+        System.out.println("Собака " + dog);
+        System.out.println("Кот " + cat);
+        System.out.println("Бумага " + paper);
 
         dog -= 3.5;
         cat -= 1.6;
         paper -= 7639;
 
-        System.out.println("Собака "+dog);
-        System.out.println("Кот "+cat);
-        System.out.println("Бумага "+ paper);
+        System.out.println("Собака " + dog);
+        System.out.println("Кот " + cat);
+        System.out.println("Бумага " + paper);
 
         var friend = 19;
         System.out.println(friend);
@@ -43,28 +43,23 @@ public class Main {
         var boxPlayer1 = 78.2;
         var boxPlayer2 = 82.7;
 
-        System.out.println(boxPlayer1+boxPlayer2+" Кг"+" Общая масса двух боксеров");
-        System.out.println(boxPlayer2-boxPlayer1+" разница между массами бойцов");
+        System.out.println(boxPlayer1 + boxPlayer2 + " Кг" + " Общая масса двух боксеров");
+        System.out.println(boxPlayer2 - boxPlayer1 + " разница между массами бойцов");
 
 
         var remainder = boxPlayer2 % boxPlayer1;
-        System.out.println( "Остаток "+ remainder);
+        System.out.println("Остаток " + remainder);
 
 
-        var onePeople = 8;
-        var sumPeople = 640;
-        var people = (sumPeople/onePeople);
-       System.out.println("Всего работников в компании  " + people +   " человек");
-
-       var newPeopleSum = people +94;
-       var newOnePeople = (sumPeople/newPeopleSum);
-       System.out.println(newOnePeople +" часов работы может быть поделено между сотрудниками");
+        var hoursPerEmployee = 8;
+        var totalHours = 640;
+        var numberOfEmployees = totalHours / hoursPerEmployee;
+        System.out.println("Всего работников в компании — " + numberOfEmployees + " человек");
 
 
-
-
-
-
+        var newEmployeeCount = numberOfEmployees + 94;
+        var newHoursPerEmployee = totalHours / newEmployeeCount;
+        System.out.println("Если в компании работает " + newEmployeeCount + " человек, то всего " + newHoursPerEmployee + " часов работы может быть поделено между сотрудниками");
 
 
     }
